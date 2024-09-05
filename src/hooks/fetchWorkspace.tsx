e3523db1e32b0108d6dcdd9c8732a8cdbab4662d
raypunzle <rayndrafattaahillah@gmail.com>
2024-09-05 13:11:49 +0700

    integration request join

--- src/hooks/fetchWorkspace.tsx
+++ src/hooks/fetchWorkspace.tsx
@@ -1,225 +1,221 @@
-  import axios from 'axios';
-  import config from '../config/baseUrl';
+import axios from 'axios';
+import config from '../config/baseUrl';
 
-  export const fetchWorkspaces = async (workspaceId: any) => {
-    try {
-      const response = await axios.get(
-        config + '/workspace/take',      {
-          headers: {
-            'Authorization': localStorage.getItem('token'),
-            'Content-Type': 'application/json',
-          }
-        }
-      );
-
-      if (response.status !== 200) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
+export const fetchWorkspaces = async (workspaceId: any) => {
+  try {
+    const response = await axios.get(
+      config + '/workspace/take', {
+      headers: {
+        'Authorization': localStorage.getItem('token'),
+        'Content-Type': 'application/json',
       }
-
-      return response.data;
-    } catch (error) {
-      console.error('Failed to fetch workspaces:', error);
-      throw error;  
     }
-  };
+    );
 
-  export const createWorkspace = async (name: any, description: any, ownerId: any) => {
-    try {
-      const response = await axios.post(
-        config + "/workspace/create",
-        { name, description, ownerId },
-        {
-          headers: {
-            'Authorization': localStorage.getItem('token'),
-            'Content-Type': 'application/json' 
-          }
-        }
-      );
-      window.location.reload();
-      return response.data;  
-    } catch (error) {
-      console.error('Failed to create workspace:', error);
-      throw error;
+    if (response.status !== 200) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
-  };
 
-  export const updateWorkspace = async (workspaceId: any, name: any, description: any, ownerId: any, isPublic: any) => {
-    try {
-        const response = await axios.put(
-            config + "/workspace/update",
-            { workspaceId, name, description, ownerId, isPublic },
-            {
-                headers: {
-                    'Authorization': localStorage.getItem('token'),
-                    'Content-Type': 'application/json' 
-                }
-            }
-        );
-        window.location.reload
-        return response.data;  
-    } catch (error) {
-        console.error('Failed to update workspace:', error);
-        throw error;
-    }
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch workspaces:', error);
+    throw error;
+  }
 };
 
-  export const deleteWorkspace = async (workspaceId: any) => {
-    try {
-      const response = await axios.delete(
-        `${config}/workspace/delete`,
-        {
-          headers: {
-            'Authorization': `${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json'
-          },
-          data: { workspaceId }
+export const createWorkspace = async (name: any, description: any, ownerId: any) => {
+  try {
+    const response = await axios.post(
+      config + "/workspace/create",
+      { name, description, ownerId },
+      {
+        headers: {
+          'Authorization': localStorage.getItem('token'),
+          'Content-Type': 'application/json'
         }
-      );
-
-      return response.data;  
-    } catch (error) {
-      console.error('Failed to delete board:', error);
-      throw error;
-    }
-  };
-
-  export const memberWorkspace = async (workspaceId: any) => {
-    try {
-      const response = await axios.get(
-        config + "/workspace/member/" + workspaceId,
-        {
-          headers: {
-            'Authorization': localStorage.getItem('token'),
-            'Content-Type': 'application/json' 
-          }
-        }
-      );
-      return response.data;  
-    } catch (error) {
-      console.error('Failed to create workspace:', error);
-      throw error;
-    }
-  };
-
-  export const joinWorkspace = async (workspaceId: any) => {
-    try {
-      const response = await axios.post(
-        config + "/workspace/join",
-        { workspaceId },
-        {
-            headers: {
-                'Authorization': localStorage.getItem('token'),
-                'Content-Type': 'application/json' 
-            }
-        }
-    );
-      window.location.reload();
-      return response.data;  
-    } catch (error) {
-      console.error('Failed to join workspace:', error);
-      throw error;
-    }
-  };
-
-  export const requstJoinWorkspace = async (workspaceId: any) => {
-    try {
-      const response = await axios.post(
-        `${config}/workspace/request-join`,
-        {
-          headers: {
-            'Authorization': `${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json'
-          },
-          data: { workspaceId }
-        }
-      );
-
-      return response.data;  
-    } catch (error) {
-      console.error('Failed to join workspace:', error);
-      throw error;
-    }
-  };
-
-  export const requstWorkspace = async (requestId: any, status: any) => {
-    try {
-      const response = await axios.put(
-        `${config}/workspace/request-join`,
-        {
-          headers: {
-            'Authorization': `${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json'
-          },
-          data: { requestId, status }
-        }
-      );
-
-      return response.data;  
-    } catch (error) {
-      console.error('Failed to join workspace:', error);
-      throw error;
-    }
-  };
-
-  export const joinWithLinkWorkspace = async ( link: any ) => {
-    try {
-      const response = await axios.post(
-        `${config}/workspace/join-with-link`,
-        {
-          headers: {
-            'Authorization': `${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json'
-          },
-          data: { link }
-        }
-      );
-
-      return response.data;  
-    } catch (error) {
-      console.error('Failed to join workspace:', error);
-      throw error;
-    }
-  };
-
-  export const generateLinkWorkspace = async (workspaceId: any) => {
-    try {
-      const response = await axios.post(
-        `${config}/workspace/generate-link`,
-        { workspaceId },
-        {
-          headers: {
-            'Authorization': localStorage.getItem('token'),
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-  
-      return response.data;  
-    } catch (error) {
-      console.error('Failed to generate join link for workspace:', error);
-      throw error;
-    }
-  };
-
-  export const joinRequestWorkspace = async (workspaceId: any) => {
-    try {
-      const response = await axios.get(
-        `${config}/workspace/join-request/${workspaceId}`,
-        {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json'
-          }
-        }
-      );
-  
-      if (response.status !== 200) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
       }
-  
-      return response.data;  
-    } catch (error) {
-      console.error('Failed to fetch join requests:', error);
-      throw error;
-    }
-  };
+    );
+    window.location.reload();
+    return response.data;
+  } catch (error) {
+    console.error('Failed to create workspace:', error);
+    throw error;
+  }
+};
+
+export const updateWorkspace = async (workspaceId: any, name: any, description: any, ownerId: any, isPublic: any) => {
+  try {
+    const response = await axios.put(
+      config + "/workspace/update",
+      { workspaceId, name, description, ownerId, isPublic },
+      {
+        headers: {
+          'Authorization': localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    window.location.reload
+    return response.data;
+  } catch (error) {
+    console.error('Failed to update workspace:', error);
+    throw error;
+  }
+};
+
+export const deleteWorkspace = async (workspaceId: any) => {
+  try {
+    const response = await axios.delete(
+      `${config}/workspace/delete`,
+      {
+        headers: {
+          'Authorization': `${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json'
+        },
+        data: { workspaceId }
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to delete board:', error);
+    throw error;
+  }
+};
+
+export const memberWorkspace = async (workspaceId: any) => {
+  try {
+    const response = await axios.get(
+      config + "/workspace/member/" + workspaceId,
+      {
+        headers: {
+          'Authorization': localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Failed to get workspace members', error);
+
+    throw error;
+  }
+};
+
+export const joinWorkspace = async (workspaceId: any) => {
+  try {
+    const response = await axios.post(
+      config + "/workspace/join",
+      { workspaceId },
+      {
+        headers: {
+          'Authorization': localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    window.location.reload();
+    return response.data;
+  } catch (error) {
+    console.error('Failed to join workspace:', error);
+    throw error;
+  }
+};
+
+export const requestJoinWorkspace = async (workspaceId: any) => {
+  try {
+    const response = await axios.post(
+      config + "/workspace/request-join",
+      { workspaceId },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`, // Include Bearer before the token
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    window.location.reload();
+    return response.data;
+  } catch (error) {
+    console.error('Failed to join workspace:', error);
+    throw error;
+  }
+};
+
+export const requstWorkspace = async (requestId: any, status: any) => {
+  try {
+    const response = await axios.put(
+      `${config}/workspace/request`,
+      {
+        headers: {
+          'Authorization': `${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json'
+        },
+        data: { requestId, status }
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to join workspace:', error);
+    throw error;
+  }
+};
+
+export const joinWithLinkWorkspace = async (link: any) => {
+  try {
+    const response = await axios.post(
+      `${config}/workspace/join-with-link`,
+      {
+        headers: {
+          'Authorization': `${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json'
+        },
+        data: { link }
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to join workspace:', error);
+    throw error;
+  }
+};
+
+export const generateLinkWorkspace = async (workspaceId: any) => {
+  try {
+    const response = await axios.post(
+      `${config}/workspace/generate-link`,
+      { workspaceId },
+      {
+        headers: {
+          'Authorization': localStorage.getItem('token'),
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to generate join link for workspace:', error);
+    throw error;
+  }
+};
+
+export const joinRequestsWorkspace = async (workspaceId: any) => {
+  try {
+    const response = await axios.get(
+      config + "/workspace/join-requests/" + workspaceId,
+      {
+        headers: {
+          'Authorization': localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetching  join request for workspace:', error);
+    throw error;
+  }
+};
